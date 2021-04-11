f628ab8e91bc8f7e1931d3f7c822098af373eb31
Anisha Jauhari <anishajauhari10@gmail.com>
2021-04-11 16:31:45 -0500

    Readme and other changes

--- asset.py
+++ asset.py
@@ -3,41 +3,62 @@ import numpy as np
 from bokeh.plotting import figure, output_file, show
 from bokeh.models import ColumnDataSource
 from bokeh.models.tools import HoverTool
+from bokeh.models.widgets import Div
+from bokeh.layouts import widgetbox
+from bokeh.transform import factor_cmap
+from bokeh.layouts import column
 
-df=pd.read_csv('./Dataset/hack_illinois_part1.csv')
-df1=pd.read_csv('./Dataset/hack_illinois_part2.csv')
+# df=pd.read_csv('./Dataset/hack_illinois_part1.csv')
+# df1=pd.read_csv('./Dataset/hack_illinois_part2.csv')
+#
+#
+# dfmerged=pd.concat([df, df1], ignore_index=True)
+def showBestAsset(dfmerged):
+    dfsort = (dfmerged.groupby(['AssetID', 'Asset type']).mean().reset_index())
+    dfsort['Efficiency (liters per hour)'] = dfsort.apply(lambda x: x['Total Fuel (Liters)'] / x['Total Hours'], axis=1)
+    print(dfsort.loc[dfsort["Efficiency (liters per hour)"].idxmax()])
+    print(dfsort.loc[dfsort["Efficiency (liters per hour)"].idxmin()])
 
+    print(dfsort["Asset type"].unique())
 
-dfmerged=pd.concat([df, df1], ignore_index=True)
-dfsort = (dfmerged.groupby(['AssetID', 'Asset type']).mean().reset_index())
-dfsort['Efficiency (liters per hour)'] = dfsort.apply(lambda x: x['Total Fuel (Liters)'] / x['Total Hours'], axis=1)
-print(dfsort.loc[dfsort["Efficiency (liters per hour)"].idxmax()])
-print(dfsort.loc[dfsort["Efficiency (liters per hour)"].idxmin()])
+    sample = dfsort.sample(1000)
+    source = ColumnDataSource(sample)
+    index_cmap = factor_cmap('Asset type', palette=['red', 'blue', 'green', 'navy', 'yellow', 'olive'],
+                             factors=sorted(dfsort['Asset type'].unique()))
 
+    output_file("gg.html")
 
-sample = dfsort.sample(20000)
-source = ColumnDataSource(sample)
+    p = figure()
+    p.circle(x='Total Fuel (Liters)', y='Total Hours',
+             source=dfsort,
+             size=5, fill_color=index_cmap, legend='Asset type')
 
+    p.title.text = 'Assets and their fuel consumption'
+    p.xaxis.axis_label = 'Average fuel consumed over a year in Liters'
+    p.yaxis.axis_label = 'Average time used over a year in hours'
 
-output_file("gg.html")
+    hover = HoverTool()
+    hover.tooltips = [
+        ('Asset ID', '@AssetID'),
+        ('Asset Type', '@{Asset type}'),
+        ('Average fuel level', '@{Fuel Level (%)}'),
+        ('Efficiency', '@{Efficiency (liters per hour)}')
+    ]
 
-p = figure()
-p.circle(x='Total Fuel (Liters)', y='Total Hours',
-         source=source,
-         size=5, color='green')
+    p.add_tools(hover)
+    p.legend.location = "top_left"
 
-p.title.text = 'Assets and their fuel consumption'
-p.xaxis.axis_label = 'Average fuel consumed over a year in Liters'
-p.yaxis.axis_label = 'Average time used over a year in hours'
+    div_exp00 = Div(
+        text=""" <b>FUEL EFFICIENCY GRAPH</b>
+                """,
+        width=300, style={'font-size': '100%'})
+    div_exp01 = Div(
+        text=""" Mileage is a key factor when it comes to analysing the performance of motor vehicles, for 
+                heavy duty vehicles the fuel consumed over time plays a similar role.
+                The graph shows the average fuel consumption for each asset in the fleet in a year. 
+                Each sphere represents an unique asset and the slope of the graph gives the average fuel consumption
+                per hour. This data allows the fleet manager to identify the most efficient assets in the fleet. The asset
+                can be closely inspected by zooming in and hovering over the spheres.""",
+        width=300)
 
-hover = HoverTool()
-hover.tooltips=[
-    ('Asset ID', '@AssetID'),
-    ('Asset Type', '@{Asset type}'),
-    ('Average fuel level', '@{Fuel Level (%)}'),
-    ('Efficiency', '@{Efficiency (liters per hour)}')
-]
-
-p.add_tools(hover)
-
-show(p)
+    show(column(div_exp00, div_exp01, p))
